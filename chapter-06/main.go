7a3bae3397fe9f0f5f7c09b838a99581b7bdcf91
Martín <chetinchog@yahoo.com>
2019-12-03 01:18:58 -0300

    Test

--- chapter-06/main.go
+++ chapter-06/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // Main function
 func main() {
 	fmt.Println("- Start -")
+	timer := time.Now().Second()
+	for index := 0; index < 28000000000; index++ {
+		if index%1000000000 == 0 {
+			fmt.Println(time.Now().Second(), " - ", time.Now().Second()-timer)
+			timer = time.Now().Second()
+		}
+	}
+	fmt.Println("- End -")
 }
